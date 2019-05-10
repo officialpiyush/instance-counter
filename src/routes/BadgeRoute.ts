9bdae4ceaa75abf6b7025bdb03122d1ff319bd8e
Piyush <bhangalepiyush@gmail.com>
2019-05-10 20:29:04 +0530

    Self Provide Badges

--- src/routes/BadgeRoute.ts
+++ src/routes/BadgeRoute.ts
@@ -1,5 +1,6 @@
 import { json } from "body-parser";
 import { Router } from "express";
+import fetch from "node-fetch";
 import { IRDB } from "../interfaces/IRDB";
 import { db } from "../utils/Database";
 
@@ -10,44 +11,54 @@ BadgeRoute.use(json());
 
 BadgeRoute.get("/tags", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.tags.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg` , res); }
+        return returnBadge(`${base}/Instances-${info.data.tags.instances}-brightgreen.svg`, res);
     });
 });
 
 BadgeRoute.get("/translator", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.translator.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg`, res); }
+        return returnBadge(`${base}/Instances-${info.data.translator.instances}-brightgreen.svg`, res);
     });
 });
 
 BadgeRoute.get("/dmonjoin", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.dmonjoin.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg`, res); }
+        return returnBadge(`${base}/Instances-${info.data.dmonjoin.instances}-brightgreen.svg`, res);
     });
 });
 
 BadgeRoute.get("/leaveserver", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.leaveserver.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg`, res); }
+        return returnBadge(`${base}/Instances-${info.data.leaveserver.instances}-brightgreen.svg`, res);
     });
 });
 
 BadgeRoute.get("/announcement", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.announcement.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg`, res); }
+        return returnBadge(`${base}/Instances-${info.data.announcement.instances}-brightgreen.svg`, res);
     });
 });
 
 BadgeRoute.get("/hastebin", (req, res) => {
     db.findOne({ id: 0 }, (err, info: IRDB) => {
-        if (err) { return res.redirect(`${base}/Error-True-red.svg`); }
-        return res.redirect(`${base}/Instances-${info.data.hastebin.instances}-brightgreen.svg`);
+        if (err) { return returnBadge(`${base}/Error-True-red.svg`, res); }
+        return returnBadge(`${base}/Instances-${info.data.hastebin.instances}-brightgreen.svg`, res);
     });
 });
+
+async function returnBadge(link: string, res: any) {
+    fetch(link).then((resp) => (resp as any).buffer()).then((buffer) => {
+        (res as any).writeHead(200, {
+            "Content-Length": buffer.length,
+            "Content-Type": "image/svg+xml",
+          });
+        (res as any).end(buffer);
+    });
+}
 
 export = BadgeRoute;
