fe11c17cc0f14ac5db8d0967b0e13e991a8e40af
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-08-04 23:08:01 +0530

    OP

--- src/routes/EmbedRoute.ts
+++ src/routes/EmbedRoute.ts
@@ -9,21 +9,15 @@ const base = "https://img.shields.io/badge";
 
 EmbedRoute.use(json());
 
-const aPlugins  = [
-    "tags",
-    "announcement",
-    "dmonjoin",
-    "hastebin",
-    "leaveserver",
-    "translator",
-    "reacttocontact",
-    "moderation",
-    "backupdb",
- ];
+let aPlugins: any = fetch("https://raw.githubusercontent.com/officialpiyush/modmail-plugins/master/plugins.json")
+    .then((res: Response) => res.json())
+    .then((j: JSON | any) => j.allowed);
+    
+async function resolvePlugin() {
+        aPlugins = await aPlugins;
+};
 
-// const aPlugins: any = fetch("https://raw.githubusercontent.com/officialpiyush/modmail-plugins/master/plugins.json")
-//     .then((res: Response) => res.json())
-//     .then((j: JSON | any) => j.allowed);
+resolvePlugin();
 
 EmbedRoute.get("/:ist", (req, res) => {
     const ist: string = (req as any).params.ist;
