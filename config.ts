66a4e6565f8ded677d98d20d47ec890825d6cfcd
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-08-06 13:58:41 +0530

    Remove config

--- config.ts
+++ config.ts
@@ -1,12 +1,13 @@
 const config = {
-    mongoURI: process.env.mongoURI ? process.env.mongoURI : "mongodb://url-c:01234url@ds115874.mlab.com:15874/url",
-    // tslint:disable-next-line: max-line-length
-    pluginsJSON: process.env.pluginsJSON ? process.env.pluginsJSON : "https://raw.githubusercontent.com/officialpiyush/modmail-plugins/master/plugins.json",
+  mongoURI: process.env.mongoURI ? process.env.mongoURI : "",
 
-    port: process.env.PORT ? process.env.PORT : 3000,
+  // example: https://raw.githubusercontent.com/officialpiyush/modmail-plugins/master/plugins.json
+  pluginsJSON: process.env.pluginsJSON ? process.env.pluginsJSON : "",
 
-    // In the format officialpiyush/modmail-plugins
-    repo: "",
+  port: process.env.PORT ? process.env.PORT : 3000,
+
+  // In the format officialpiyush/modmail-plugins
+  repo: "",
 };
 
 export default config;
